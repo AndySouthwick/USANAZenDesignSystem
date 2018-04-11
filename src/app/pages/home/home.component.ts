9349f7b18745d54ca94ce5e6f5c701e4fe241d7a
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-04-11 15:28:57 -0600

    changed title

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.data = {
-      title: 'USANA UX DIGITAL DESIGN SYSTEM'
+      title: 'USANA ZEN DIGITAL DESIGN SYSTEM'
     };
   }
 
